2d2744612bdc93ad9a509715d262c32e662543f7
Sean Carlin <spcarlin@hotmail.com>
2017-12-10 19:52:36 +0000

    Initial Commit

---
+++ FireBaseTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FireBaseTest
+//
+//  Created by Apple on 10/12/2017.
+//  Copyright © 2017 com.spcarlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FireBaseTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FireBaseTest
+//
+//  Created by Apple on 10/12/2017.
+//  Copyright © 2017 com.spcarlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
